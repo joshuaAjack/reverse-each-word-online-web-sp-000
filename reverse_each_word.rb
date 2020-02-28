2837beb6043a07911d274261b9966812d2bd8d07
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-28 03:46:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence)
-  reversed = sentence.split(" ").collect do |word|
+def reverse_each_word(string)
+  reversed = string.split(" ").collect do |word|
   word.reverse
 end
   reversed.join(" ")
